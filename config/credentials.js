fba0216f79285e3af4770d40fd304037e6deb130
wulichenyang <943934049@qq.com>
2018-12-28 11:58:31 +0800

    Backend of suny forum

---
+++ config/credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GITHUB_CLIENT_ID: '46caa7d262dc55d94365',
+  GITHUB_CLIENT_SECRET: 'd9fed8dd85c30d02db21a2d239478739b3ea7adf',
+  GITHUB_CALLBACK_URL: 'http://localhost:3000/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/sunyForum',
+};
